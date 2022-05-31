edc7f827c7aba14a3d5c8185dd17a1ef99866ec5
costajf <costajf@proton.me>
2022-05-31 12:47:12 -0500

    Solve problems 07 to 09.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,11 @@
-const ftoc = function() {
-
+const ftoc = function(faren) {
+  let celsius = (faren - 32) * (5 / 9);
+  return +celsius.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function(cels) {
+  let farenheit =  (cels * (9/5)) + 32;
+  return +farenheit.toFixed(1);
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(n1, n2) {
+	return n1 + n2;
 };
 
-const subtract = function() {
-	
+const subtract = function(n1, n2) {
+	return n1 - n2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  result = 0;
+	for (let index = 0; index < array.length; index++) {
+     result = result + array[index];    
+  } return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  result = 1;
+	for (let index = 0; index < array.length; index++) {
+     result = result * array[index];    
+  } return result;
 };
 
-const power = function() {
-	
+const power = function(n1, n2) {
+	return n1 ** n2;
 };
 
-const factorial = function() {
-	
+const factorial = function(n1) {
+	if (n1 == 0) {  
+    return 1;  
+    }  
+  else {  
+    return n1 * factorial( n1 - 1 );  
+    }  
 };
 
 // Do not edit below this line
