0d8165a9ac3bac7a2d7bb660c254db10b4a96a4a
costajf <costajf@proton.me>
2022-05-31 12:45:19 -0500

    Solve problems 07 to 09.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+    words = words.toLowerCase();
+    words = words.replace(/[^\w\s\']|_/g, "").replace(/\s+/g, "");
+    let backwards = words.split("").reverse().join("");
+    console.log(backwards);
+    if (backwards == words) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
